d8c01a0e17ddff363aece19210f56e430b7d9a21
Jane Byrne <janebyrne92@gmail.com>
2020-01-27 19:08:05 -0600

    NOW sections 1-3

---
+++ src/Tests/JobTest.java
@@ -0,0 +1,21 @@
+package Tests;
+import jdk.jfr.StackTrace;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.launchcode.*;
+import org.launchcode.techjobs_oo.Job;
+
+
+public class JobTest {
+
+    @Test
+
+    public void noTwoIdsAreEqual() {
+        Job testJob1 = new Job();
+        Job testJob2 = new Job();
+
+        assertFalse(testJob1.)
+    }
+
+}
